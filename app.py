acefb2e6db294801e99fbbf845cfcf526426b1a1
BinaM122 <77073970+BinaM122@users.noreply.github.com>
2026-02-16 14:39:57 -0500

    Refactor game functions and fix variable issues
    Refactored functions to SideOne and SideTwo for Brooklyn Nets and Cleveland Cavs. Fixed variable naming and typo in reboundsTotal.

--- app.py
+++ app.py
@@ -3,21 +3,8 @@ from nba_api.stats.endpoints import LeagueGameFinder
 from nba_api.stats.endpoints import BoxScoreTraditionalV3
 
 
-def one():
-    gamefinder = LeagueGameFinder(team_id_nullable=1610612751)
-    games = gamefinder.get_data_frames()[0]
-    latest_game_id = games.iloc[0]["GAME_ID"]
-    print(latest_game_id)
-    print(games.head()) 
-  
-
-def two():
-    nba_teams = teams.get_teams()
-
-    nets = [t for t in nba_teams if t["full_name"] == "Brooklyn Nets"][0]
-    print(nets)
-
-def three():
+# Brooklyn Nets
+def SideOne():
     gamefinder = LeagueGameFinder(team_id_nullable=1610612751)
     games = gamefinder.get_data_frames()[0]
 
@@ -40,4 +27,34 @@ def three():
     )
 
 
-three()
+# Cleveland Cavs 
+def SideTwo():
+    gamefinder = LeagueGameFinder(team_id_nullable=1610612739)
+    games = gamefinder.get_data_frames()[0]
+
+    latest_game_id = games.iloc[0]["GAME_ID"]
+
+    boxscore = BoxScoreTraditionalV3(game_id=latest_game_id)
+    player_stats = boxscore.player_stats.get_data_frame()
+
+    cavs_stats = player_stats[player_stats["teamId"] == 1610612739]
+    cavs_stats["player"] = (  # Changed from "Player" to "player"
+        cavs_stats["firstName"] + " " + cavs_stats["familyName"]
+    )
+
+    print(
+        cavs_stats[
+            ["player", "points", "reboundsTotal", "assists"]  # Fixed "rebountTotal" typo
+        ]
+    )
+
+
+def HeadToHead():
+    print("--------------HEADTOHEAD------------")
+    print("teamOne:")
+    SideOne()
+    print("\nteamTwo:")
+    SideTwo()
+
+
+HeadToHead()  
